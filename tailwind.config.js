afb27d81085303a90738f856559a8f467d1e23da
shivamsuyal <shivamsuyal25@gmail.com>
2024-02-14 19:50:11 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,21 @@ module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
     extend: {
-      // screens:{
-      //   'md': '900px',
-      //   'sm-l':'425px',
-      //   'sm-m':'375px',
-      //   'sm-s':'320px',
-      //   'md-m':'1208px'
-      // },
+      screens:{
+        'md': '900px',
+        'sm-l':'425px',
+        'sm-m':'375px',
+        'sm-s':'320px',
+        'md-m':'1208px'
+      },
       fontFamily:{
         "overpass": ["Overpass",'sans-serif'],
         "montserrat": ["Montserrat",'sans-serif'],
       },
-      // colors:{
-      //   "button-color": "#FD6363",
-      //   "dark-text-color": "#1B1C1E",
-      //   "light-text-color": "#8A8A8A",
-      //   "dark-bg-color": "#151516",
-      //   "border-color": "#3F4349",
-      //   "contactFormBg": "#141316",
-      // }
+      colors:{
+        "main-color-dark": "#00338D",
+        "main-color-light": "#0D4AB2",
+      }
     },
   },
   plugins: [
